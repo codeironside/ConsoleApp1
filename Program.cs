6b547e7d104a89014c43d3d4df77073d059eed10
codeironside <fury25423@gmail.com>
2022-01-09 01:44:14 +0100

    nothing much to see her just me testing some codes out
    the question
    
    Tobi has a graph showing several expected housing values over the next few years. She has to buy the house in one year and sell it in another year, all while losing money. She wants to keep her financial losses to a minimum.
    
    Consider the following array, which represents the house price for each year: arr = [10,5,3]. Purchasing in year 2 (at 5) and selling in year 3 results in a minimum loss for her (at 3). In this example, the loss is 5 - 3 = 2.
    
    Complete the function to determine and return the minimum loss given an array of house prices.
    
    Sample Input:
    
    10 5 3
    
    Sample Output:
    
    2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int[] yearPrices = {10,5,3 };
+int firstYear, secondYear, thirdYear;
+firstYear = yearPrices[0];
+secondYear = yearPrices[1];
+thirdYear = yearPrices[2];
+
+if(firstYear - secondYear < firstYear -thirdYear) 
+{
+    if (firstYear - secondYear < secondYear - thirdYear)
+    {
+        string firstYear1, secondYear1;
+        string answer;
+        int answer1;
+        answer1 = firstYear - secondYear;
+        answer = answer1.ToString();
+        firstYear1 = firstYear.ToString();
+        secondYear1 = secondYear.ToString();
+        Console.WriteLine("the minimum loss is " +  firstYear1 + " - " + secondYear1 + " = " + answer);
+
+
+
+    }
+    else
+    {
+        string secondYear1, thirdYear1;
+        string answer;
+        int answer1;
+        answer1 = secondYear - thirdYear;
+        answer = answer1.ToString();
+        thirdYear1 = thirdYear.ToString();
+        secondYear1 = secondYear.ToString();
+        Console.WriteLine("the minimum loss is " +secondYear1 + " - " + thirdYear1 + " = " + answer);
+    }
+}
+else
+{
+    string firstYear1, thirdYear1;
+    string answer;
+    int answer1;
+    answer1 = firstYear - thirdYear;
+    answer = answer1.ToString();
+    thirdYear1 = thirdYear.ToString();
+    firstYear1 = firstYear.ToString();
+    Console.WriteLine("the minimum loss is " + firstYear1 + " - " + thirdYear1 + " = " + answer);
+}
